a702a125fb496879904b29eaa84948f77ae83bf9
kenty02 <42216299+kenty02@users.noreply.github.com>
2024-01-02 19:58:06 +0900

    fix(SampleFileViewer): no wrap content

--- frontend/src/components/SampleFileViewer.tsx
+++ frontend/src/components/SampleFileViewer.tsx
@@ -41,7 +41,7 @@ export function SampleFileViewer() {
 					<div>サンプルファイルの内容</div>
 					<div
 						className={
-							"font-mono whitespace-pre-wrap bg-gray-100 p-2 w-full h-96 overflow-auto" +
+							"font-mono whitespace-pre bg-gray-100 p-2 w-full h-96 overflow-auto" +
 							" border-2 border-gray-300 rounded-md outline-none"
 						}
 					>
