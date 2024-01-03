7e10eaa3b57b046985c96c02cf309bddf8eb816c
kenty02 <42216299+kenty02@users.noreply.github.com>
2024-01-04 01:13:56 +0900

    chore: run sentry frontend only on prod

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -24,6 +24,7 @@ Sentry.init({
 	replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
 	replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 	environment: import.meta.env.VITE_SENTRY_ENVIRONMENT,
+	enabled: import.meta.env.PROD,
 });
 async function enableMocking() {
 	if (process.env.NODE_ENV !== "development") {
